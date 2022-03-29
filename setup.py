700aa9755a91b7e4017f061df76d3a6017565099
Romanas Sonkinas <romanass@gmail.com>
2022-03-29 15:17:58 +0300

    Upgrade black to the latest and greatest

--- setup.py
+++ setup.py
@@ -70,7 +70,12 @@ setup(
             "pytest-aiohttp==0.3.0",
             "pytest-asyncio==0.18.2",
         ],
-        "develop": ["black==20.8b1", "flake8==3.8.4", "isort==5.6.4", "mypy==0.770"],
+        "develop": [
+            "black==22.3.0",
+            "flake8==3.8.4",
+            "isort==5.6.4",
+            "mypy==0.770",
+        ],
     },
     python_requires=">=3.8",
     classifiers=[
