fe08d5178182c3c1fd6076df8674582ced054049
Romanas Sonkinas <romanas@crate.io>
2022-08-23 10:57:47 +0100

    LICENSE CHANGE
    AGPL -> Apache 2.0

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 # CrateDB Kubernetes Operator
-# Copyright (C) 2020 Crate.IO GmbH
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed to Crate.IO GmbH ("Crate") under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  Crate licenses
+# this file to you under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# However, if you have executed another commercial license agreement
+# with Crate these terms will supersede the license and you may use the
+# software solely pursuant to the terms of the relevant commercial agreement.
 
 
 import os
@@ -36,7 +41,7 @@ setup(
     author="Crate.IO GmbH",
     author_email="office@crate.io",
     description="CrateDB Kubernetes Operator",
-    license="AGPLv3",
+    license="Apache License 2.0",
     long_description=read("README.rst"),
     long_description_content_type="text/x-rst",
     packages=find_namespace_packages(include=["crate.*"]),
@@ -79,8 +84,8 @@ setup(
     },
     python_requires=">=3.8",
     classifiers=[
-        "Development Status :: 1 - Beta",
-        "License :: OSI Approved :: GNU Affero General Public License v3"
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
