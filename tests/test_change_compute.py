b391e071df46d5227bb9779d635f875db63fb19b
Juan Pardo <juan@crate.io>
2022-08-23 11:20:22 +0200

    fixup! fixup! fixup! fixup! Update tests/test_change_plan.py

--- tests/test_change_compute.py
+++ tests/test_change_compute.py
@@ -19,11 +19,11 @@ from unittest import mock
 
 import pytest
 from kubernetes_asyncio.client import CoreV1Api, CustomObjectsApi
-from webhooks import WebhookChangeComputePayload
 
 from crate.operator.change_compute import generate_body_patch
 from crate.operator.constants import API_GROUP, RESOURCE_CRATEDB
 from crate.operator.cratedb import connection_factory
+from crate.operator.webhooks import WebhookChangeComputePayload
 
 from .utils import (
     DEFAULT_TIMEOUT,
