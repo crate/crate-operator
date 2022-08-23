68d7f3656eb351c87e27dc3ad205c383127c9ef3
Juan Pardo <juan@crate.io>
2022-08-23 11:07:12 +0200

    fixup! fixup! Update tests/test_change_plan.py

--- tests/test_change_compute.py
+++ tests/test_change_compute.py
@@ -13,11 +13,14 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+import logging
 from typing import Set
+from unittest import mock
 
 import pytest
+from change_compute import generate_body_patch
 from kubernetes_asyncio.client import CoreV1Api, CustomObjectsApi
+from webhooks import WebhookChangeComputePayload
 
 from crate.operator.constants import API_GROUP, RESOURCE_CRATEDB
 from crate.operator.cratedb import connection_factory
@@ -45,12 +48,76 @@ async def do_pod_ids_exist(core: CoreV1Api, namespace: str, pod_ids: Set[str]) -
 
 @pytest.mark.k8s
 @pytest.mark.asyncio
-async def test_up_cpu_and_ram(faker, namespace, kopf_runner, api_client):
+@pytest.mark.parametrize(
+    "set_initial_request_resources, cpu_request, memory_request, cpu_limit,"
+    "memory_limit",
+    [
+        # Test a change in cpu and memory with requests = limits
+        (False, 1, "5Gi", 1, "5Gi"),
+        # Test a change in cpu and memory from req = limits to req != limits
+        (False, 1, "3Gi", 3, "5Gi"),
+        # Test a change in cpu and memory from req != limits to req = limits
+        (True, 1, "5Gi", 1, "5Gi"),
+    ],
+)
+async def test_change_cpu_and_ram(
+    set_initial_request_resources,
+    cpu_request,
+    memory_request,
+    cpu_limit,
+    memory_limit,
+    faker,
+    namespace,
+    kopf_runner,
+    api_client,
+):
+    """
+    Tests that the cratedb resource changes in cpu/memory requests/limits are properly
+    passed on to the statefulset.
+    Note we cannot test the affinity in a test cluster.
+    """
+    body_changes = [
+        {
+            "op": "replace",
+            "path": "/spec/nodes/data/0/resources/limits/cpu",
+            "value": cpu_limit,
+        },
+        {
+            "op": "replace",
+            "path": "/spec/nodes/data/0/resources/limits/memory",
+            "value": memory_limit,
+        },
+    ]
+    if cpu_request != cpu_limit:
+        body_changes.append(
+            {
+                "op": "replace",
+                "path": "/spec/nodes/data/0/resources/requests/cpu",
+                "value": cpu_request,
+            },
+        )
+    if memory_request != memory_limit:
+        body_changes.append(
+            {
+                "op": "replace",
+                "path": "/spec/nodes/data/0/resources/requests/memory",
+                "value": memory_request,
+            },
+        )
+
     coapi = CustomObjectsApi(api_client)
     core = CoreV1Api(api_client)
     name = faker.domain_word()
-    # start_cluster creates a cluster with cpu=2 and memory=4Gi per node
-    host, password = await start_cluster(name, namespace, core, coapi, 3, "5.0.0")
+    if set_initial_request_resources:
+        # Start a cluster with requests set to half the original limits
+        initial_requests = {"cpu": 1, "memory": "2Gi"}
+        host, password = await start_cluster(
+            name, namespace, core, coapi, 3, "5.0.0", resource_requests=initial_requests
+        )
+    else:
+        # Start a cluster without requests, just the standard limits of cpu=2 and
+        # memory=4Gi per node
+        host, password = await start_cluster(name, namespace, core, coapi, 3, "5.0.0")
 
     await assert_wait_for(
         True,
@@ -94,18 +161,7 @@ async def test_up_cpu_and_ram(faker, namespace, kopf_runner, api_client):
         plural=RESOURCE_CRATEDB,
         namespace=namespace.metadata.name,
         name=name,
-        body=[
-            {
-                "op": "replace",
-                "path": "/spec/nodes/data/0/resources/limits/cpu",
-                "value": 1,  # Decrease from 2
-            },
-            {
-                "op": "replace",
-                "path": "/spec/nodes/data/0/resources/limits/memory",
-                "value": "5Gi",  # Increase from 4Gi
-            },
-        ],
+        body=body_changes,
     )
 
     await assert_wait_for(
@@ -183,7 +239,58 @@ async def test_up_cpu_and_ram(faker, namespace, kopf_runner, api_client):
     for p in pods.items:
         for c in p.spec.containers:
             if c.name == "crate":
-                assert c.resources.limits["cpu"] == str(1)
-                assert c.resources.limits["memory"] == "5Gi"
-                assert c.resources.requests["cpu"] == str(1)
-                assert c.resources.requests["memory"] == "5Gi"
+                assert c.resources.limits["cpu"] == str(cpu_limit)
+                assert c.resources.limits["memory"] == memory_limit
+                assert c.resources.requests["cpu"] == str(cpu_request)
+                assert c.resources.requests["memory"] == memory_request
+
+
+@pytest.mark.parametrize(
+    "old_cpu_limit, old_memory_limit, old_cpu_request, old_memory_request, "
+    "new_cpu_limit, new_memory_limit, new_cpu_request, new_memory_request",
+    [
+        # Test no requests
+        (1, "2Gi", None, None, 3, "5Gi", None, None),
+        # Test requests set
+        (1, "2Gi", None, None, 3, "5Gi", 5, "8Gi"),
+    ],
+)
+def test_generate_body_patch(
+    old_cpu_limit,
+    old_memory_limit,
+    old_cpu_request,
+    old_memory_request,
+    new_cpu_limit,
+    new_memory_limit,
+    new_cpu_request,
+    new_memory_request,
+    faker,
+):
+    plan_change_data = WebhookChangeComputePayload(
+        old_cpu_limit=old_cpu_limit,
+        old_memory_limit=old_memory_limit,
+        old_cpu_request=old_cpu_request,
+        old_memory_request=old_memory_request,
+        new_cpu_limit=new_cpu_limit,
+        new_memory_limit=new_memory_limit,
+        new_cpu_request=new_cpu_request,
+        new_memory_request=new_memory_request,
+    )
+
+    name = faker.domain_word()
+    with mock.patch("crate.operator.create.config.TESTING", False):
+        body = generate_body_patch(name, plan_change_data, logging.getLogger(__name__))
+
+    resources = body["spec"]["template"]["spec"]["containers"][0]["resources"]
+    assert resources["limits"]["cpu"] == new_cpu_limit
+    assert resources["limits"]["memory"] == new_memory_limit
+
+    assert resources["requests"]["cpu"] == new_cpu_request or new_cpu_limit
+    assert resources["requests"]["memory"] == new_memory_request or new_memory_limit
+
+    if new_cpu_request or new_memory_request:
+        assert body["spec"]["template"]["spec"]["affinity"].node_affinity is not None
+    else:
+        assert (
+            body["spec"]["template"]["spec"]["affinity"].pod_anti_affinity is not None
+        )
