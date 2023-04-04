3c1b8184ed2313435e0f3abeb51f34baa051f77f
Juan Pardo <juan@crate.io>
2023-04-04 17:52:13 +0200

    fixup! fixup! fixup! fixup! Suspending a cluster now deletes the load balancer.

--- tests/test_scale.py
+++ tests/test_scale.py
@@ -256,7 +256,7 @@ async def test_suspend_resume_cluster(
         namespace.metadata.name,
         f"{KOPF_STATE_STORE_PREFIX}/cluster_update",
         err_msg="Scaling down has not finished",
-        timeout=DEFAULT_TIMEOUT * 2,
+        timeout=DEFAULT_TIMEOUT * 5,
     )
 
     await assert_wait_for(
