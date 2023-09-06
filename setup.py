2ac3d0f3222ecfae53d899486c43a5ed7e673e68
Andreas Motl <andreas.motl@crate.io>
2023-09-06 18:08:49 +0200

    Maintenance: Add support for Python 3.11

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     install_requires=[
         "aiopg==1.4.0",
         "bitmath==1.3.3.1",
-        "kopf==1.35.6",
+        "kopf==1.36.2",
         "kubernetes-asyncio==31.1.0",
         "PyYAML<7.0",
         "prometheus_client==0.21.1",
@@ -85,7 +85,10 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     use_scm_version=True,
 )
