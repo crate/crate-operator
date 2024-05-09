be385fe488eb755974d67c9cbceecdd47b276606
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-05-09 11:54:23 +0200

    AB#1261: Use not the internal load balancer and setup build

--- crate/operator/create.py
+++ crate/operator/create.py
@@ -954,7 +954,7 @@ def get_data_service(
                 ),
             ],
             selector={LABEL_COMPONENT: "cratedb", LABEL_NAME: name},
-            type="LoadBalancer",
+            type="ClusterIP",
             external_traffic_policy="Local",
             load_balancer_source_ranges=source_ranges if source_ranges else None,
         ),
