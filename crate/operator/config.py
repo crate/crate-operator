d73509120ba2ff712f16e76e71ec3fdc3c8ce96a
Shyukri Shyukriev <shyukri.shyukriev@crate.io>
2026-09-02 15:37:39 +0300

    Update SQL Exporter to 0.24.8

--- crate/operator/config.py
+++ crate/operator/config.py
@@ -178,7 +178,7 @@ class Config:
     PROMETHEUS_PORT: int = 8080
 
     #: The sql_exporter image to use
-    SQL_EXPORTER_IMAGE: str = "burningalchemist/sql_exporter:0.24.6"
+    SQL_EXPORTER_IMAGE: str = "burningalchemist/sql_exporter:0.24.8"
 
     #: Name of the secret containing credentials to access the source
     #: backup when restoring a snapshot.
